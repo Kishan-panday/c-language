3b512beb2e8d1128308e27448fb7dfe42681cbfd
Kishan-Panday <kishanpandey4463@gmail.com>
2024-08-12 10:44:07 +0530

    pattern

---
+++ pattern.c
@@ -0,0 +1,24 @@
+# include<stdio.h>
+int main(){
+    int n;
+printf("Enter the number of rows:");
+scanf("%d",&n);
+
+for(int i=1; i<=n;i++){
+ 
+for (int j = 1; j<=n; j++)
+{
+
+if (i==1 || i==4 || j==1 || j==4)
+{
+  printf("* ");
+}
+else
+ printf("  ");
+ 
+}
+printf("\n");
+}
+
+    return 0;
+}
