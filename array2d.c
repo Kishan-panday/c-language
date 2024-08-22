316929453f474199a4b6846c0ab3f4b0bee12b4b
Kishan-Panday <kishanpandey4463@gmail.com>
2024-08-23 00:00:59 +0530

    array2D

---
+++ array2d.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    printf("Enter the number of rows :");
+    scanf("%d",&r);
+    printf("Enter the number of column :");
+    scanf("%d",&c);
+    printf("Enter the number of elements in array :\n");
+   
+   int arr[r][c];
+    for(int i=0; i<r; i++){
+
+        for(int j=0; j<c; j++){
+            scanf("%d",&arr[i][j]);
+        }
+   
+    }
+       printf("\n");
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
