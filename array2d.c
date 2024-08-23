a5b5b3cdc465edd24f613cea8ab6dc75e2408c74
Kishan-Panday <kishanpandey4463@gmail.com>
2024-08-24 00:38:13 +0530

    array2D

--- array2d.c
+++ array2d.c
@@ -23,5 +23,14 @@ int main(){
         printf("\n");
     }
     
+    int sum =0;
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            sum +=arr[i][j];
+        }
+        printf("\n");
+    }
+    printf("the sum of the array is %d",sum);
+    
     return 0;
 }
