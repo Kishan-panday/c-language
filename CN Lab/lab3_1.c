6385cf8f7422488cebf823511cda08a60976a7aa
Kishan-Panday <kishanpandey4463@gmail.com>
2025-08-01 23:53:01 +0530

    reverse

--- CN Lab/lab3_1.c
+++ CN Lab/lab3_1.c
@@ -1,20 +1,17 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-void swap(int *num1,int *num2){
-    int temp=*num1;
-    *num1=*num2;
-    *num2= temp;
-     printf("After swap%d and %d\n",*num1,*num2);
+#include <stdio.h>
+void swap(int a, int b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    printf("Inside swap function: a = %d, b = %d\n", a, b);
 }
-int main(){
-    int x,y;
-    printf("Enter first number to swap:");
-    scanf("%d\n",&x);
-    printf("Enter second number to swap:");
-    scanf("%d\n",&y);
-    printf("Before swap%d and %d\n",x,y);
-    swap(&x,&y);
-   
-return 0;
+int main() {
+    int x = 5, y = 10;
+
+    printf("Before swap: x = %d, y = %d\n", x, y);
+    swap(x, y);
+    printf("After swap (main): x = %d, y = %d\n", x, y);
+
+    return 0;
 }
